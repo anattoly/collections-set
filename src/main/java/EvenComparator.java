3abe02de4d2725a05b251c954d457dc025fd1de6
anattoly <anattoly.k@gmail.com>
2019-07-14 20:48:06 +0300

    refactored from unary operator to binary

--- src/main/java/EvenComparator.java
+++ src/main/java/EvenComparator.java
@@ -5,12 +5,12 @@ import java.util.Comparator;
 public class EvenComparator implements Comparator<Integer> {
     @Override
     public int compare(Integer o1, Integer o2) {
-        if (o1 % 2 != 0 & o2 % 2 != 0 | o1 % 2 == 0 & o2 % 2 == 0) {
-            return o1.compareTo(o2);
-        } else if (o1 % 2 == 0 & o2 % 2 != 0) {
+        if (o1 % 2 != 0 && o2 % 2 == 0) {
+            return 1;
+        } else if (o1 % 2 == 0 && o2 % 2 != 0) {
             return -1;
         } else {
-            return 1;
+            return o1.compareTo(o2);
         }
     }
 }
